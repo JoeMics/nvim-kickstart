9f730ed715c3e7c8a32b5f1136264fa8508420dc
JoeMics <46177831+JoeMics@users.noreply.github.com>
2024-03-08 09:23:11 -0800

    change colorscheme to terafox

--- init.lua
+++ init.lua
@@ -742,14 +742,22 @@ require('lazy').setup {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  {
+    'EdenEast/nightfox.nvim',
+    config = function()
+      vim.cmd.colorscheme 'terafox'
+    end,
+  },
+
+  -- unused, but I like them
+  { 'ellisonleao/gruvbox.nvim' },
+  { 'catppuccin/nvim', as = 'catppuccin' },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
