f96368d9978a52807ee285bdadd9791478c2cd8b
JoeMics <46177831+JoeMics@users.noreply.github.com>
2024-09-05 11:50:06 -0700

    keymaps: fugitive

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,3 +1,10 @@
 return {
   'tpope/vim-fugitive',
+  config = function()
+    -- keymaps
+    vim.keymap.set('n', '<leader>gs', '<cmd>G<CR>', { desc = '[G]it [S]tatus' })
+    vim.keymap.set('n', '<leader>gcr', '<cmd>Git commit<CR>', { desc = '[G]it [C]ommit with [M]essage' })
+    vim.keymap.set('n', '<leader>gcm', '<cmd>Git commit --amend<CR>', { desc = '[G]it [C]ommit - [A]mmend' })
+    vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>', { desc = '[G]it [P]ush' })
+  end,
 }
