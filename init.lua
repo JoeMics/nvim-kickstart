d8d17a9b3c2ca87a0af001ebad1c26a2d247427d
JoeMics <46177831+JoeMics@users.noreply.github.com>
2024-09-05 11:31:56 -0700

    add transparency to colorscheme git push

--- init.lua
+++ init.lua
@@ -796,8 +796,19 @@ require('lazy').setup {
   },
   {
     'EdenEast/nightfox.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
     config = function()
       vim.cmd.colorscheme 'terafox'
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
 
